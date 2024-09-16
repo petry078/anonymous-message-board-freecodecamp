8e4997a5da807abe4f16873efc7c8f85c6b8c142
petry <petry@pop-os.localdomain>
2024-09-16 13:04:14 -0300

    render

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 'use strict';
 require('dotenv').config();
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
 
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
@@ -17,62 +12,59 @@ const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Log all requests
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log(`${Date.now()}: ${req.method} ${req.path} - ${req.ip}`);
-  }
-  next();
-});
 
-// Commented out options use helmet defaults.
-const helmetConfig = {
-  contentSecurityPolicy: {
-    directives: {
-      'default-src': ['\'self\''],
-      'base-uri': ['\'self\''],
-      'block-all-mixed-content': [],
-      'font-src': ['\'self\'', 'https:'],
-      'frame-ancestors': ['\'self\''],
-      'img-src': ['\'self\'', 'cdn.freecodecamp.org'],
-      'object-src': ['\'none\''],
-      'script-src': ['\'self\'', 'code.jquery.com', '\'unsafe-inline\''],
-      'style-src': ['\'self\'', 'https:', '\'unsafe-inline\''],
-      'upgrade-insecure-requests': [],
-    },
-  },
-  // expectCt: false,
-  referrerPolicy: {policy: 'same-origin'},
-  // hsts: false,
-  // hsts: {maxAge: 7776000},
-  // noSniff: false,
-  dnsPrefetchControl: {allow: false},
-  // ieNoOpen: false,
-  frameguard: {action: 'SAMEORIGIN'},
-  // permittedCrossDomainPolicies: false,
-  // hidePoweredBy: false,
-  // xssFilter: false,
-};
-app.use(helmet(helmetConfig));
+
+// ***************************************************** //
+// helmet 
+const helmet = require("helmet")
+
+app.use(
+  helmet.frameguard({
+    action: "sameorigin",
+  })
+);
+
+app.use(helmet.dnsPrefetchControl({
+  allow: false
+}))
+
+app.use(helmet.referrerPolicy({
+  policy: ["same-origin"],
+}))
+
+
+// ***************************************************** //
+// to connect mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env['DB'])
+  .then(() => {
+    console.log('Mongoose connected')
+  })
+  .catch((err) => {
+    console.log('error', err)
+  })
+
+
+// ***************************************************** //
 
 //Sample front-end
 app.route('/b/:board/')
-  .get((req, res) => {
+  .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/board.html');
   });
 app.route('/b/:board/:threadid')
-  .get((req, res) => {
+  .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/thread.html');
   });
 
 //Index page (static HTML)
 app.route('/')
-  .get((req, res) => {
+  .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -83,40 +75,23 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Setup server and use SSL if enabled
-let server;
-let PORT;
-if (!!process.env.ENABLE_SSL) {
-  const certOptions = {
-    key: fs.readFileSync(path.resolve('certs/server.key')),
-    cert: fs.readFileSync(path.resolve('certs/server.crt')),
-  };
-
-  server = https.createServer(certOptions, app);
-  PORT = process.env.PORT || 8443;
-} else {
-  server = app;
-  PORT = process.env.PORT || 3000;
-}
-
 //Start our server and tests!
-const listener = server.listen(PORT, () => {
-  console.log(`Listening on port ${listener.address().port}`);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
   if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(() => {
+    setTimeout(function () {
       try {
         runner.run();
       } catch (e) {
-        const error = e;
         console.log('Tests are not valid:');
-        console.log(error);
+        console.error(e);
       }
     }, 1500);
   }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,237 +2,174 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const { Thread, getThreadId, getReplyId } = require("../model/messageBoard")
+
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+const threadPostData = { board: "test", text: "test", delete_password: "test" }
+let replyData = { text: "test", delete_password: "test", board: "test" }
 
-  const testBoard = 'functional_test_board';
-  let threadId = 'fakeId';
-  let replyId = 'fakeId';
+suite('Functional Tests', function () {
 
-  test('Creating a new thread: POST request to /api/threads/{board}', (done) => {
+  test("#1 POST: Creating a new thread", function (done) {
     chai.request(server)
-      .post(`/api/threads/${testBoard}`)
-      .send({
-        'board': testBoard,
-        'text': 'functional testing thread',
-        'delete_password': 'mypassword',
+      .post("/api/threads/test")
+      .send(threadPostData)
+      .end(async (err, res) => {
+        assert.equal(res.status, 200)
+        assert.isDefined(res.body._id)
+        assert.isArray(res.body.replies)
       })
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          // Check that we redirected to the message board
-          assert.include(res.text, `h1 id='boardTitle'`);
-        }
-        done();
-      });
-  });
+    done()
+  })
 
-  test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', (done) => {
-    const threadObjectKeys = [
-      '_id',
-      'text',
-      'created_on',
-      'bumped_on',
-      'replies',
-      'replycount'];
-
+  test("#2 GET: Viewing the 10 most recent threads with 3 replies each", function (done) {
     chai.request(server)
-      .get(`/api/threads/${testBoard}`)
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          assert.isArray(res.body, 'Response should be an Array');
-          assert.hasAllKeys(res.body[0], threadObjectKeys, 'Missing key(s) from thread object');
-          assert.doesNotHaveAnyKeys(res.body[0], ['reported', 'delete_password']);
-          assert.isArray(res.body[0]['replies'], 'replies is not an array');
-          // Save threadId and replyId
-          threadId = res.body[0]['_id'];
-        }
-        done();
-      });
-  });
-
-  test('Creating a new reply: POST request to /api/replies/{board}', (done) => {
-    chai.request(server)
-      .post(`/api/replies/${testBoard}`)
-      .send({
-        'thread_id': threadId,
-        'text': 'functional testing thread reply',
-        'delete_password': 'mypassword',
+      .get("/api/threads/test")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.isArray(res.body)
+        assert.isObject(res.body[0])
+        assert.isDefined(res.body[0].text)
+        assert.isDefined(res.body[0].created_on)
+        assert.isDefined(res.body[0].bumped_on)
+        assert.isArray(res.body[0].replies)
+        assert.isBelow(res.body[0].replies.length, 4)
+        done()
       })
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          // Check that we redirected to the thread page
-          assert.include(res.text, `h1 id='threadTitle'`);
-        }
-        done();
-      });
-  });
+  })
 
-  test('Viewing a single thread with all replies: GET request to /api/replies/{board}', (done) => {
-    const threadObjectKeys = ['_id', 'text', 'created_on', 'bumped_on', 'replies'];
-    const replyObjectKeys = ['_id', 'text', 'created_on'];
-
+  test("#3 DELETE: Deleting a thread with the incorrect password", async function () {
+    thread = await getThreadId("test", "test")
     chai.request(server)
-      .get(`/api/replies/${testBoard}`)
-      .query({'thread_id': threadId})
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          assert.isObject(res.body, 'Response should be an object');
-          assert.hasAllKeys(res.body, threadObjectKeys, 'Missing key(s) from thread object');
-          assert.doesNotHaveAnyKeys(res.body, ['reported', 'delete_password']);
-          assert.isArray(res.body['replies'], 'replies in not an array');
-          assert.hasAllKeys(res.body['replies'][0], replyObjectKeys, 'Missing key(s) from reply object');
-          assert.doesNotHaveAnyKeys(res.body['replies'][0], ['reported', 'delete_password']);
-          replyId = res.body['replies'][0]['_id'];
-        }
-        done();
-      });
-  });
-
-  test('Reporting a thread: PUT request to /api/threads/{board}', (done) => {
-    chai.request(server)
-      .put(`/api/threads/${testBoard}`)
+      .delete("/api/threads/test")
       .send({
-        'thread_id': threadId,
+        ...threadPostData,
+        thread_id: thread._id.toString(),
+        delete_password: "incorrect"
       })
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          assert.isString(res.text, 'Response should be a string');
-          assert.equal(res.text, 'reported');
-        }
-        done();
-      });
-  });
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, "incorrect password")
+      })
+  })
 
-  test('Reporting a reply: PUT request to /api/replies/{board}', (done) => {
+  test("#5 PUT: Reporting a thread", async function () {
+    thread = await getThreadId("test", "test")
     chai.request(server)
-      .put(`/api/replies/${testBoard}`)
+      .put("/api/threads/test")
       .send({
-        'thread_id': threadId,
-        'reply_id': replyId,
+        ...threadPostData,
+        thread_id: thread._id.toString(),
       })
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          assert.fail('Error running test');
-        } else {
-          assert.equal(res.status, 200);
-          assert.isString(res.text, 'Response should be a string');
-          assert.equal(res.text, 'reported');
-        }
-        done();
-      });
-  });
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, "reported")
+      })
+  })
 
-  test(
-    'Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password',
-    (done) => {
-      chai.request(server)
-        .delete(`/api/replies/${testBoard}`)
-        .send({
-          'thread_id': threadId,
-          'reply_id': replyId,
-          'delete_password': 'badpassword',
-        })
-        .end((err, res) => {
-          if (err) {
-            console.error(err);
-            assert.fail('Error running test');
-          } else {
-            assert.equal(res.status, 200);
-            assert.isString(res.text, 'Response should be a string');
-            assert.equal(res.text, 'incorrect password');
-          }
-          done();
-        });
-    });
+  // // ------------------------------------------------------------- //
 
-  test(
-    'Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password',
-    (done) => {
-      chai.request(server)
-        .delete(`/api/replies/${testBoard}`)
-        .send({
-          'thread_id': threadId,
-          'reply_id': replyId,
-          'delete_password': 'mypassword',
-        })
-        .end((err, res) => {
-          if (err) {
-            console.error(err);
-            assert.fail('Error running test');
-          } else {
-            assert.equal(res.status, 200);
-            assert.isString(res.text, 'Response should be a string');
-            assert.equal(res.text, 'success');
-          }
-          done();
-        });
-    });
+  test("#6 POST: Creating a new reply", async function () {
+    thread = await getThreadId("test", "test")
+    chai.request(server)
+      .post("/api/replies/test")
+      .send({
+        ...replyData,
+        thread_id: thread._id.toString(),
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.isDefined(res.body._id)
+        assert.isArray(res.body.replies)
+        assert.isObject(res.body.replies[0])
+        assert.isDefined(res.body.replies[0].text)
+        assert.isDefined(res.body.replies[0].created_on)
+      })
+  })
 
-  test(
-    'Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password',
-    (done) => {
-      chai.request(server)
-        .delete(`/api/threads/${testBoard}`)
-        .send({
-          'thread_id': threadId,
-          'delete_password': 'badpassword',
-        })
-        .end((err, res) => {
-          if (err) {
-            console.error(err);
-            assert.fail('Error running test');
-          } else {
-            assert.equal(res.status, 200);
-            assert.isString(res.text, 'Response should be a string');
-            assert.equal(res.text, 'incorrect password');
-          }
-          done();
-        });
-    });
+  test("#7 GET: Viewing a single thread with all replies", async function () {
+    thread = await getThreadId("test", "test")
+    chai.request(server)
+      .get("/api/replies/test" + "?thread_id=" + thread._id.toString())
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.isObject(res.body)
+        assert.isDefined(res.body.text)
+        assert.isDefined(res.body.created_on)
+        assert.isDefined(res.body.bumped_on)
+        assert.isArray(res.body.replies)
+        assert.isObject(res.body.replies[0])
+        assert.isDefined(res.body.replies[0].text)
+        assert.isDefined(res.body.replies[0].created_on)
+      })
+  })
 
-  test(
-    'Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password',
-    (done) => {
-      chai.request(server)
-        .delete(`/api/threads/${testBoard}`)
-        .send({
-          'thread_id': threadId,
-          'delete_password': 'mypassword',
-        })
-        .end((err, res) => {
-          if (err) {
-            console.error(err);
-            assert.fail('Error running test');
-          } else {
-            assert.equal(res.status, 200);
-            assert.isString(res.text, 'Response should be a string');
-            assert.equal(res.text, 'success');
-          }
-          done();
-        });
-    });
+  test("#8 DELETE: Deleting a reply with the incorrect password", async function () {
+    thread = await getThreadId("test", "test")
+    let threadId = thread._id.toString()
+    reply = await getReplyId(threadId)
+    chai.request(server)
+      .delete("/api/replies/test")
+      .send({
+        thread_id: threadId,
+        reply_id: reply._id.toString(),
+        delete_password: "incorrect",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, "incorrect password")
+      })
+  })
+
+  test("#9 DELETE: Deleting a reply with the correct password", async function () {
+    thread = await getThreadId("test", "test")
+    let threadId = thread._id.toString()
+    reply = await getReplyId(threadId)
+    chai.request(server)
+      .delete("/api/replies/test")
+      .send({
+        thread_id: threadId,
+        reply_id: reply._id.toString(),
+        delete_password: reply.delete_password || "test",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, "success")
+      })
+  })
+
+  test("#10 PUT: Reporting a reply", async function () {
+    thread = await getThreadId("test", "test")
+    let threadId = thread._id.toString()
+    reply = await getReplyId(threadId)
+    chai.request(server)
+      .put("/api/replies/test")
+      .send({
+        thread_id: threadId,
+        reply_id: reply._id.toString(),
+        board: "test"
+      })
+      .end(function (err, res) {
+        assert.equal(res.text, "reported")
+        assert.equal(res.status, 200)
+      })
+  })
+
+
+  test("#4 DELETE: Deleting a thread with the correct password", async function () {
+    thread = await getThreadId("test", "test")
+    chai.request(server)
+      .delete("/api/threads/test")
+      .send({
+        ...threadPostData,
+        thread_id: thread._id.toString(),
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, "success")
+      })
+  })
 
 });
